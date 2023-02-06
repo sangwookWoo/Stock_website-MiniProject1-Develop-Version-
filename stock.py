7d85c59ef43fc83db0d582b12342bdd0905c16b3
SangwookWoo <wjddm3@naver.com>
2023-02-06 23:38:25 +0900

    보조지표, 매수매도 그래프 3종 추가

--- stock.py
+++ stock.py
@@ -4,24 +4,8 @@ from pykrx import stock
 import pandas as pd
 import os
 from pytz import timezone
+import plotly.express as px
 
-
-# yf.pdr_override()
-# now = datetime.now()
-# end_day = datetime(now.year, now.month, now.day)
-# start_day = end_day - timedelta(days = 365)
-
-# kospi_df = stock.get_index_fundamental(start_day.strftime('%Y%m%d'), end_day.strftime('%Y%m%d'), "1001") # 코스피
-
-# st.metric('코스피 지수', value = kospi_df.iloc[-1, 0], delta = str(kospi_df.iloc[-1, 1]) + '%')
-
-# tab1, tab2 = st.tabs(["지수 현황", "등락 폭 비교"])
-# with tab1:
-#     st.line_chart(kospi_df['종가'], use_container_width = True)
-# with tab2:
-#     pass
-
-# 안녕헬로
 
 def stock_price(stock_name, code, start_day, end_day):
     '''
@@ -34,8 +18,6 @@ def stock_price(stock_name, code, start_day, end_day):
     except Exception as E:
         st.write('stock_price 함수 오류')
         st.write(E)
-
-
 
 def main():
     
@@ -72,6 +54,38 @@ def main():
         with col1:
             # 시각화
             stock_price(stock_name, code, start_day, end_day)
+        with col2:
+            st.write(' ')
+            st.write(' ')
+            st.markdown('###### 최근 종목 보조지표')
+            st.write(' ')
+            st.write(' ')
+            
+            st.dataframe(stock.get_market_fundamental(start_day, end_day, code).tail(1).T, use_container_width= True)
+        
+        number = st.slider('거래일 기준 며칠의 정보를 조회하시겠습니까?', 0, 365, 30)
+        start_day = end_day - timedelta(days = number)
+        
+        
+        col3, col4, col5 = st.columns(3)
+        maesoo = pd.DataFrame(stock.get_market_trading_volume_by_date(start_day, end_day, code, on='매수').sum()).reset_index()[:-1]
+        maedo = pd.DataFrame(stock.get_market_trading_volume_by_date(start_day, end_day, code, on='매도').sum()).reset_index()[:-1]
+        with col3:
+            fig1 = px.pie(maesoo, values = 0, names = 'index', title = '순매수 점유율')
+            st.plotly_chart(fig1, use_container_width= True)
+        with col4:
+            fig2 = px.pie(maedo, values = 0, names = 'index', title = '순매도 점유율')
+            st.plotly_chart(fig2, use_container_width= True)
+        with col5:
+            st.write(' ')
+            st.write(' ')
+            st.markdown('###### 매수/매도 비교 그래프')
+            st.write(' ')
+            data_mae = pd.merge(maesoo, maedo, how = 'inner', on = 'index')
+            data_mae.columns = ['주체','매수','매도']
+            st.bar_chart(data_mae, x = '주체', y = ['매수', '매도'], use_container_width= True)
+            
+            
             
     elif stock_name not in tuple(base_df['cooperation']):
         st.write('❗정확한 종목명을 입력하세요❗')
